5dc535a074764f69f52b07c347cbe64802286748
vishlvirus <103377103+vishlvirus@users.noreply.github.com>
2022-12-29 11:37:38 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
-apt-get update -y
-apt-get upgrade -y
+yum update -y
+yum upgrade -y
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-apt-get install -y nodejs
+yum install -y nodejs
 nodejs -v
 npm -v
 npm install -g @angular/cli@13
 cd /angular-realworld-example-app.git
 npm install
 ng build
-apt-get install nginx -y
+yum install nginx -y
 cd /var/www/html/
 rm -rf index.nginx-debian.html
 cd /home/ubuntu/angular-realworld-example-app/dist
