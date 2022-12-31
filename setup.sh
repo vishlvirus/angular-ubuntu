efe5f4b3e0ed4c40f86a633497cbf5f60dfd9955
vishlvirus <103377103+vishlvirus@users.noreply.github.com>
2022-12-31 11:44:30 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,6 +12,7 @@ cd /var/www/html/
 rm -rf index.nginx-debian.html
 cd /home/ubuntu/angular-realworld-example-app/dist
 cp -r * /var/www/html/
+chmod 777 *
 cd /
 systemctl restart nginx
 apt-get update -y
